f4a8349a0bfbc66781f33ecee948084de6cd0a00
dmigasiuk <dmigasiuk@gmail.com>
2015-06-11 13:46:51 +0200

    2

--- HelloWorld/src/com/mypackage/Main.java
+++ HelloWorld/src/com/mypackage/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World! abc");
+		System.out.println("0.1");
+		System.out.println("Hello World!");
 
 	}
 
