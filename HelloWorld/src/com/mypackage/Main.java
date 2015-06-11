c4aa5f810af68918721efe79cbcdd3c5775bc36d
dmigasiuk <dmigasiuk@gmail.com>
2015-06-11 15:37:09 +0200

    maven

--- HelloWorld/src/com/mypackage/Main.java
+++ HelloWorld/src/com/mypackage/Main.java
@@ -1,7 +1,12 @@
 package com.mypackage;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
 
+	static Logger log = LogManager.getLogger(Main.class);
+	
 	public static void main(String[] args) {
 		
 		System.out.println("0.1");
